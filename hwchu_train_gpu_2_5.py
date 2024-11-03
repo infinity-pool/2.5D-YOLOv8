af365e36a07fa808d400369c79769022ab359cb4
infinity-pool <hjchoo98@gmail.com>
2024-11-03 18:29:14 +0900

    train on different conditions

--- hwchu_train_gpu_2_5.py
+++ hwchu_train_gpu_2_5.py
@@ -1,32 +1,37 @@
 from ultralytics import YOLO
 import cv2
 
-model_path = 'yolov8n.pt'
-model = YOLO("yolov8n_2_5.yaml", task="detect_2_5").load(model_path)
-device = 'cuda'
+test_model_scales = ['n', 's', 'm', 'l']
+epochs = 40
 
-results = model.train(epochs=30,
-    device=device,
-    data="nuScenes_2_5.yaml",
-    project='yolov8_2_5',
-    # save_dir="/root/data/hwchu/yolov8_results",
-    # workers=0
-    ) # nuScenes_2_5.yaml training
-# results = model.train(epochs=5, device=device, data="coco8.yaml") # coco8.yaml training
+for m_scale in test_model_scales:
+    for epoch in epochs:
+        model_path = f'yolov8{m_scale}.pt'
+        model = YOLO(f"yolov8{m_scale}_2_5.yaml", task="detect_2_5").load(model_path)
+        device = 'cuda'
 
-# print('[results[0].boxes.data.shape]')
-# print(results[0].boxes.data.shape)
+        results = model.train(epochs=epoch,
+            device=device,
+            data="nuScenes_2_5.yaml",
+            project='yolov8_2_5',
+            # save_dir="/root/data/hwchu/yolov8_results",
+            # workers=0
+            )
 
-saved_best_model = YOLO(f'{results.save_dir}/weights/best.pt')
-# saved_best_model = YOLO('/root/data/hwchu/yolov8_results/weights/best.pt')
+        saved_best_model = YOLO(f'{results.save_dir}/weights/best.pt')
 
-# inference_results = saved_best_model('./ultralytics/assets/bus.jpg')
-inference_results = saved_best_model('../datasets/nuScenes_2_5/images/val/n008-2018-08-30-15-31-50-0400__CAM_BACK__1535657649037558.jpg')
+        # inference_results = saved_best_model('./ultralytics/assets/bus.jpg')
+        inference_results = saved_best_model('../datasets/nuScenes_2_5/images/val/n008-2018-08-30-15-31-50-0400__CAM_BACK__1535657649037558.jpg')
 
-'''결과 파일로 저장'''
-with open(f"{results.save_dir}/result_2_5_train.txt", "w") as file:
-    file.write(str(inference_results[0].boxes))
+        '''결과 파일로 저장'''
+        with open(f"{results.save_dir}/result_2_5_train.txt", "w") as file:
+            file.write(str(inference_results[0].boxes))
+            file.write('[BOXES]\n')
+            file.write(str(inference_results[0].boxes) + '\n\n')
+            file.write('[DISTS]\n')
+            file.write(str(inference_results[0].dists))
 
-'''Visualize'''
-plots = inference_results[0].plot(show=False)
-cv2.imwrite(f'{results.save_dir}/result_2_5_train.jpg', plots)
+        '''Visualize'''
+        plots = inference_results[0].plot(show=False)
+        cv2.imwrite(f'{results.save_dir}/result_2_5_train.jpg', plots)
+
